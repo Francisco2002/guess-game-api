c8dbb9bb04ff078c5ca4e953b845b026ad2ea3f2
Francisco Silva <franciscosilva22082002@gmail.com>
2025-04-28 11:18:17 -0300

    feat: initial setup
    - connection with mongoose
    - connection with socket
    - welcome route

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express, { json, urlencoded } from 'express';
+import { createServer } from "http";
+import { connect } from 'mongoose';
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}))
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+  }
+});
+
+io.on("connection", socket => {
+  socket.emit("connected");
+});
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/guessgame";
+
+connect(mongoUrl).then(() => console.log('MongoDB conectado'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" })
+});
+
+httpServer.listen(3000, () => console.log('API rodando na porta 3000'));
